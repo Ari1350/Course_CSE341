580601cfb9aadf22a44a58113b66255ec6538be3
Guadalupe <ariasperaltag@gmail.com>
2026-07-31 23:15:46 -0400

    change

--- W3_project2/swagger.js
+++ W3_project2/swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'API for managing books and authors - Project 2 Part 1',
   },
   host: 'localhost:8080',
-  schemes: ['https', 'http'],
+  schemes: ['https'],
 };
 
 const outputFile = './swagger-output.json';
